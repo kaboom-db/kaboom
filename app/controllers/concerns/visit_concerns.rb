41237a6ee6a961927aecbfa77c3e3c57deffb0c9
Tanveer Najib <tanveernajib@gmail.com>
2023-07-03 19:41:59 +0100

    Lint

--- app/controllers/concerns/visit_concerns.rb
+++ app/controllers/concerns/visit_concerns.rb
@@ -4,7 +4,7 @@ module VisitConcerns
   extend ActiveSupport::Concern
 
   def add_visit(user:, visited:)
-    return if user.present? && Visit.where(user:, visited:).where('created_at >= ?', 5.minutes.ago).exists?
+    return if user.present? && Visit.where(user:, visited:).where("created_at >= ?", 5.minutes.ago).exists?
 
     Visit.create!(user:, visited:)
   end
