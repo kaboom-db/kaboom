7a7eb0c08de9cd6a75b081668d47852056cf0797
Tanveer Najib <tanveernajib@gmail.com>
2023-07-06 21:13:30 +0100

    Import issues when syncing comic

--- app/sidekiq/sync_job.rb
+++ app/sidekiq/sync_job.rb
@@ -5,6 +5,7 @@ class SyncJob
     trending_comics = Comic.trending.limit(20)
     trending_comics.each do |comic|
       comic.sync
+      comic.import_issues
     end
   end
 end
--- spec/sidekiq/sync_job_spec.rb
+++ spec/sidekiq/sync_job_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe SyncJob, type: :job do
   it "syncs trending comics" do
     comic = instance_double(Comic)
     expect(Comic).to receive_message_chain(:trending, :limit).and_return([comic])
-    expect(comic).to receive(:sync).and_return(true)
+    expect(comic).to receive(:sync)
+    expect(comic).to receive(:import_issues)
     SyncJob.new.perform
   end
 end
