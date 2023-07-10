f2c018a8d41a3df5c2afa71dc59e1829f16c8843
Tanveer Najib <tanveernajib@gmail.com>
2023-07-10 19:19:46 +0100

    Redo local time controller and start history page

--- app/javascript/controllers/local_time_controller.ts
+++ app/javascript/controllers/local_time_controller.ts
@@ -2,14 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="local-time"
 export default class extends Controller {
+  static values = { utc: String }
+
+  declare utcValue: string
+
   MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
   connect () {
-    const times = document.querySelectorAll<HTMLElement>('div[data-time]')
-    times.forEach((element) => {
-      const { time } = element.dataset
-      const local = new Date(Date.parse(time || ''))
-      element.innerText = `${local.getDate()} ${this.MONTHS[local.getMonth()]} ${local.getFullYear()} ${local.toLocaleTimeString().slice(0, -3)}`
-    })
+    const local = new Date(Date.parse(this.utcValue))
+    this.element.textContent = `${local.getDate()} ${this.MONTHS[local.getMonth()]} ${local.getFullYear()} ${local.toLocaleTimeString().slice(0, -3)}`
   }
 }
