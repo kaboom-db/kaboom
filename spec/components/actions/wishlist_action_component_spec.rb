afc5251da4a5ca0efc466292ed49745e2e27aeea
Tanveer Najib <tanveernajib@gmail.com>
2023-08-04 14:54:47 +0100

    Add spec for visual edge case

--- spec/components/actions/wishlist_action_component_spec.rb
+++ spec/components/actions/wishlist_action_component_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe Actions::WishlistActionComponent, type: :component do
   it "renders the icon" do
     expect(page).to have_css "i.fa-cake-candles"
   end
+
+  context "when resource is an Issue" do
+    it "does not render any borders on the sides" do
+      expect(page).to have_css "button.border-x-none"
+    end
+  end
+
+  context "when resouce is not an issue" do
+    let(:resource) { FactoryBot.create(:comic) }
+
+    it "renders a border on the left but not the right" do
+      expect(page).to have_css "button.border-l-2"
+      expect(page).to have_css "button.border-r-none"
+    end
+  end
 end
