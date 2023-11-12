1fee7ebb794540092139a793bd91873458af4fb3
Tanveer Najib <tanveernajib@gmail.com>
2023-11-12 12:15:03 +0000

    Use ResourceTile more

--- app/components/resource_tile_component.rb
+++ app/components/resource_tile_component.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class ResourceTileComponent < ViewComponent::Base
-  def initialize(resource:, resource_path:)
+  def initialize(resource:, resource_path:, read: false, collected: false, wishlisted: false, favourited: false)
     @resource = resource
     @resource_path = resource_path
+    @read = read
+    @collected = collected
+    @wishlisted = wishlisted
+    @favourited = favourited
   end
 end
