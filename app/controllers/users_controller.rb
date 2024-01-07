2d6cf517d2da92a6d6c4e0a432260c9d4fe54b6c
Tanveer Najib <tanveernajib@gmail.com>
2024-01-07 10:05:44 +0000

    Show more on the user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def show
+    @history = @user.read_issues.order(read_at: :desc).limit(6)
+    @deck = @user.incompleted_comics.take(6)
+    @favourite_comics = @user.favourited_comics.limit(6)
+    @completed_comics = @user.completed_comics.take(12)
+    @wishlisted = @user.wishlisted_comics.limit(12)
   end
 
   private
