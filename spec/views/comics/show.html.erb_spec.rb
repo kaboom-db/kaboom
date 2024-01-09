df4bee7ba48e1e162d0bc7e66d4df98ec9379642
Tanveer Najib <tanveernajib@gmail.com>
2024-01-09 21:52:51 +0000

    Only render batch read issues when user is signed in

--- spec/views/comics/show.html.erb_spec.rb
+++ spec/views/comics/show.html.erb_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe "comics/show", type: :view do
     render
     assert_select "h1.text-2xl", text: "Demon Slayer"
   end
+
+  context "when user is signed in" do
+    before do
+      def view.user_signed_in? = true
+    end
+
+    it "renders the batch read issues section" do
+      render
+      assert_select "summary.font-bold", text: "Batch read issues"
+    end
+  end
+
+  context "when the user is not signed in" do
+    before do
+      def view.user_signed_in? = false
+    end
+
+    it "does not render the batch read issues section" do
+      render
+      assert_select "summary.font-bold", text: "Batch read issues", count: 0
+    end
+  end
 end
