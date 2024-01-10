6e86289bf111b9a40de54b1045c34d84c1d83c8b
Tanveer Najib <tanveernajib@gmail.com>
2024-01-10 20:29:58 +0000

    Add smtp settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => Rails.application.credentials.sendgrid_apikey, # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'kaboom.rocks',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
