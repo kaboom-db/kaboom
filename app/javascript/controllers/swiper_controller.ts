7b78111092a971f611cfd1b3af9b605028279323
Tanveer Najib <tanveernajib@gmail.com>
2024-01-11 21:13:16 +0000

    Fix swiper warning when 0 or 1 elements

--- app/javascript/controllers/swiper_controller.ts
+++ app/javascript/controllers/swiper_controller.ts
@@ -3,18 +3,20 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="swiper"
 export default class extends Controller {
-  static targets = ['container', 'next', 'prev', 'pagination']
+  static targets = ['container', 'next', 'prev', 'pagination', 'slides']
 
   declare readonly containerTarget: HTMLElement
   declare readonly nextTarget: HTMLElement
   declare readonly prevTarget: HTMLElement
   declare readonly paginationTarget: HTMLElement
 
+  declare readonly slidesTargets: HTMLElement[]
+
   declare swiper: Swiper
 
   connect () {
     this.swiper = new Swiper(this.containerTarget, {
-      loop: true,
+      loop: this.slidesTargets.length > 1,
       navigation: {
         nextEl: this.nextTarget,
         prevEl: this.prevTarget
