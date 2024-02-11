69af46c4434478da4e4617fda8c1e90bd30504c5
Tanveer Najib <48864420+crxssed7@users.noreply.github.com>
2024-02-11 20:20:01 +0000

    Wording

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def user_required
     return if user_signed_in?
-    flash[:notice] = "You need to be logged in access this page."
+    flash[:notice] = "You need to be logged in to access this page."
     redirect_to new_session_path(User)
   end
 
