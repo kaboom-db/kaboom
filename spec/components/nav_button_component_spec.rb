18b20873a62bc04be7618453b6e7f57a6aa1d69a
Tanveer Najib <tanveernajib@gmail.com>
2024-02-24 17:06:50 +0000

    Add spec for nav button method

--- spec/components/nav_button_component_spec.rb
+++ spec/components/nav_button_component_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe NavButtonComponent, type: :component do
     render_inline(described_class.new(href: "/path", icon: "test", text: "Hey There!"))
     expect(page).to have_link href: "/path"
     expect(page).to have_css ".fa-test"
+    expect(page).to have_css "a[data-turbo-method='get']"
     expect(page).not_to have_css ".border-2"
   end
 
@@ -14,6 +15,13 @@ RSpec.describe NavButtonComponent, type: :component do
     it "renders the button in active state" do
       render_inline(described_class.new(href: "/path", icon: "test", text: "Hey There!", active: true))
       expect(page).to have_css ".border-2"
+    end
+  end
+
+  context "when a method is specified" do
+    it "uses the specified method" do
+      render_inline(described_class.new(href: "/path", icon: "test", text: "Hey There!", method: :delete))
+      expect(page).to have_css "a[data-turbo-method='delete']"
     end
   end
 end
