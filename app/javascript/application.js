9d81c04e4d99f4f99f69c16017c510ec0e14a6ef
Tanveer Najib <tanveernajib@gmail.com>
2024-03-10 15:25:33 +0000

    Lint JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,13 @@
 import '@hotwired/turbo-rails'
 import './controllers'
 
-// Do not prefetch links when hovering over them
-Turbo.session.linkPrefetchObserver.stop()
-
 import Swiper from 'swiper'
 import { Pagination, Navigation, FreeMode, Mousewheel } from 'swiper/modules'
 // import Swiper and modules styles
 import 'swiper/css/bundle'
+
+// Do not prefetch links when hovering over them
+/* eslint-disable no-undef */
+Turbo.session.linkPrefetchObserver.stop()
 
 Swiper.use([Pagination, Navigation, FreeMode, Mousewheel])
