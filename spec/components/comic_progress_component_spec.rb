e1b2079d0951e22e54fe021ed14bf0716996ae3f
Tanveer Najib <tanveernajib@gmail.com>
2024-03-10 17:47:06 +0000

    Use issue image in progress component

--- spec/components/comic_progress_component_spec.rb
+++ spec/components/comic_progress_component_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe ComicProgressComponent, type: :component do
 
   context "when there is a next issue" do
     it "renders the component with next issue text" do
-      comic = FactoryBot.create(:comic, name: "Test Comic", count_of_issues: 2, image: "/path/to/image.png")
+      comic = FactoryBot.create(:comic, name: "Test Comic", count_of_issues: 2)
       issue = FactoryBot.create(:issue, comic:, absolute_number: 1, issue_number: "1")
-      FactoryBot.create(:issue, comic:, absolute_number: 2, issue_number: "2")
+      issue2 = FactoryBot.create(:issue, comic:, absolute_number: 2, issue_number: "2", image: "/path/to/image.png")
       current_user = FactoryBot.create(:user)
       FactoryBot.create(:read_issue, issue:, user: current_user)
       render_inline(described_class.new(comic:, current_user:))
       expect(page).to have_css "img[src='/path/to/image.png']"
       expect(page).to have_css "a[href='#{comic_path(comic)}']"
+      expect(page).to have_css "a[href='#{comic_issue_path(issue2, comic_id: comic)}']"
       expect(page).to have_content "Test Comic"
       expect(page).to have_content "Next: Issue #2"
       expect(page).to have_content "50%"
