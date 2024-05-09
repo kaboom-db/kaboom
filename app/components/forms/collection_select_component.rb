9c73cae6208773e2418674059ab82379cb7b5f90
Tanveer Najib <tanveernajib@gmail.com>
2024-05-09 20:19:38 +0100

    Add custom label text for genre field for comics form

--- app/components/forms/collection_select_component.rb
+++ app/components/forms/collection_select_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Forms::CollectionSelectComponent < ViewComponent::Base
-  def initialize(form:, field:, options:, identifier: :to_s, include_blank: true, multiple: false)
+  def initialize(form:, field:, options:, identifier: :to_s, include_blank: true, multiple: false, label_text: nil)
     @form = form
     @field = field
     @options = options
     @identifier = identifier
     @include_blank = include_blank
     @multiple = multiple
+    @label_text = label_text
   end
 end
--- spec/components/forms/collection_select_component_spec.rb
+++ spec/components/forms/collection_select_component_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe Forms::CollectionSelectComponent, type: :component do
       expect(page).to have_content "Control + Click to select/deselect an option"
     end
   end
+
+  context "when label text is provided" do
+    it "renders the custom text" do
+      comic = Comic.new
+      view = ActionView::Base.empty
+      form = ActionView::Helpers::FormBuilder.new(:comic, comic, view, {})
+      render_inline(described_class.new(form:, field: :comic_type, options: Comic::TYPES, label_text: "This is some custom text"))
+      expect(page).to have_css "label", text: "This is some custom text"
+    end
+  end
 end
