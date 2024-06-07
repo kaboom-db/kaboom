c5b06b443ea7184e9633484274a56dcd8ce374ed
Tanveer Najib <tanveernajib@gmail.com>
2024-06-07 23:04:12 +0100

    Rails 7.1.3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  config.action_dispatch.show_exceptions = :none
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
@@ -37,6 +37,9 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
+
+  # The default :async adapter can cause hanging when running tests with Active Storage.
+  config.active_job.queue_adapter = :test
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
