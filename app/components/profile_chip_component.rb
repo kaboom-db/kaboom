47ba16693692b457422c3cfc7af38d0f4a13f740
Tanveer Najib <tanveernajib@gmail.com>
2024-06-15 13:55:04 +0100

    Properly render the user in follow dialogs

--- app/components/profile_chip_component.rb
+++ app/components/profile_chip_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ProfileChipComponent < ViewComponent::Base
-  def initialize(user:)
+  def initialize(user:, in_dialog: false)
     @user = user
+    @in_dialog = in_dialog
   end
 end
--- spec/components/profile_chip_component_spec.rb
+++ spec/components/profile_chip_component_spec.rb
@@ -3,11 +3,20 @@
 require "rails_helper"
 
 RSpec.describe ProfileChipComponent, type: :component do
+  let(:user) { FactoryBot.create(:user, username: "TestUser2", created_at: Time.new(2024, 1, 1, 10, 0, 0, "+00:00")) }
+
   it "renders the component" do
-    user = FactoryBot.create(:user, username: "TestUser", created_at: Time.new(2024, 1, 1, 10, 0, 0, "+00:00"))
     render_inline(described_class.new(user:))
     expect(page).to have_content "TestUser"
     expect(page).to have_content "Member since: 1 Jan 2024 10:00"
     expect(page).to have_css "img[src='#{user.avatar}']"
+    expect(page).to have_css "div[class='hidden md:block']"
+  end
+
+  context "when in_dialog is true" do
+    it "does not render the responsive version" do
+      render_inline(described_class.new(user:, in_dialog: true))
+      expect(page).to have_css "div[class='block']"
+    end
   end
 end
