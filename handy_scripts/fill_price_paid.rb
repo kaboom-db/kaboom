14b78f3203361fb4f5dbe43b8625f18e5afc5f2b
Tanveer Najib <tanveernajib@gmail.com>
2024-07-09 20:49:40 +0100

    Update handy script

--- handy_scripts/fill_price_paid.rb
+++ handy_scripts/fill_price_paid.rb
@@ -1,13 +1,16 @@
 def collect
-  comic = Comic.find(5) # Find the comic
+  comic = Comic.find(1) # Find the comic
   user = User.find_by!(username: "crxssed") # Find yourself
   prices = [
     # Enter your individual volume prices here
   ]
   issues = comic.ordered_issues
+  cis = []
   prices.each_with_index do |price, index|
     issue = issues[index]
-    ci = CollectedIssue.where(user:, issue:)
+    ci = CollectedIssue.where(user:, issue:).first
     ci.update(price_paid: price)
+    cis << ci
   end
+  cis
 end
