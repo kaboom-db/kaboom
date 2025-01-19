e893865ca8f163bce83f998c473035fdf763588d
Tanveer Najib <tanveernajib@gmail.com>
2025-01-19 18:08:40 +0000

    Specs for star rating component

--- spec/components/star_rating_component_spec.rb
+++ spec/components/star_rating_component_spec.rb
@@ -3,13 +3,20 @@
 require "rails_helper"
 
 RSpec.describe StarRatingComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when current_user is not present" do
+    it "does not render" do
+      render_inline(described_class.new(current_user: nil, rate_url: ""))
+      expect(page).to have_css "div[data-controller='star-rating']", count: 0
+    end
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  context "when current_user is present" do
+    it "renders the component" do
+      current_user = FactoryBot.create(:user)
+      render_inline(described_class.new(current_user:, rate_url: "/url/for/rating", rating: 25))
+      expect(page).to have_css "div[data-controller='star-rating']"
+      expect(page).to have_css "div[data-star-rating-current-score-value='25']"
+      expect(page).to have_css "div[data-star-rating-url-value='/url/for/rating']"
+    end
+  end
 end
